023d3ea6ad0685612bd93849af9c83007d1fa054
rangers-store <dapxy96@gmail.com>
2026-03-12 22:16:47 +0700

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,17 @@
+local Place_ID_With_Matching_Luarmor_ID = {
+    [13643807539] = "https://api.luarmor.net/files/v4/loaders/16fbd70fa4f67e17014e3a949c9d57bf.lua"; -- South Bronx
+};
+
+if not Place_ID_With_Matching_Luarmor_ID[game.PlaceId] then
+    game.Players.LocalPlayer:Kick("Rangers.rawr | This game is not supported!")
+end
+
+if not getfenv().script_key then
+    game.Players.LocalPlayer:Kick("Rangers.rawr | Key not found!")
+end
+
+script_key = getfenv().script_key
+
+writefile('rangers_key.txt', getfenv().script_key)
+
+loadstring(game:HttpGet(Place_ID_With_Matching_Luarmor_ID[game.PlaceId]))()
